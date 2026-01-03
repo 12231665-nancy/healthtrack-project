b6e1eddfe2a64d49df3ae07d7db68cd0f9cbc873
Nancy Nasr <nancy@example.com>
2026-01-03 18:26:23 +0200

    Update API URLs and login config for deployed backend

--- project/src/pages/Login.js
+++ project/src/pages/Login.js
@@ -12,7 +12,7 @@ function Login({ setUser }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("${API_BASE_URL}/login",{
+      const res = await axios.post(`${API_BASE_URL}/login`,{
         email: email,
         password: password
 
